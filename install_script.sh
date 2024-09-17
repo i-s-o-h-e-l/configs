b766d9ebf9f1642ec4990c32ceb2268a52d7afb1
kirill <kirill.aistov@yandex.ru>
2024-09-17 21:13:15 +0300

    misc updates

--- install_script.sh
+++ install_script.sh
@@ -7,10 +7,12 @@ if [ $USING_MSYS2 ]; then
     echo "Installing MSYS2 packages"
     pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain git make mingw-w64-x86_64-cmake mingw-w64-x86_64-clang mingw-w64-x86_64-clang-tools-extra mingw-w64-x86_64-python mingw-w64-x86_64-python-pip zsh mingw-w64-x86_64-ripgrep mingw-w64-x86_64-fzf mingw-w64-x86_64-bat mingw-w64-x86_64-fd mingw-w64-x86_64-neovim mingw-w64-x86_64-nodejs
 
-elif [ $USING_UBUNTU ] ; then   
+elif [ $USING_UBUNTU ]; then
     echo "Installing Ubuntu packages"
     # TODO add other packages
-    sudo apt install zsh tmux bat
+    # NOTE might need to install fzf via https://github.com/junegunn/fzf?tab=readme-ov-file#using-git if the version in apt is too old
+    sudo apt install zsh tmux bat ripgrep fd-find nodejs npm fzf
+    sudo snap install nvim # latest version
 fi
 
 if [ ! -d $HOME/.oh-my-zsh ]; then
